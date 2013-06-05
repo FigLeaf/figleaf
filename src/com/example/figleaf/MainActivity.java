ed96a5ad68549f579418b33d4656ad840c008de7
lafwind <lafwind@gmail.com>
2013-06-05 21:36:56 +0800

    keyguard

--- src/com/example/figleaf/MainActivity.java
+++ src/com/example/figleaf/MainActivity.java
@@ -41,194 +41,194 @@ public class MainActivity extends Activity implements OnClickListener {
     private final String LOCK_SCREEN_ON_OFF = "lock_screen_on_off";
     
     BaseAdapter myAdapter = new BaseAdapter(){             //为listview写适配器
-		
-		public int getCount() {
-			if(nameArray!= null){		    //如果姓名数组不为空
-				return nameArray.length;
-			}
-			else {
-				return 0;					//如果姓名数组为空则返回0
-			}
-		}
-		
-		public Object getItem(int arg0) {
-			return null;
-		}
-		
-		public long getItemId(int arg0) {
-			return 0;
-		}
-		
-		public View getView(int position, View convertView, ViewGroup parent) {   //适配器李添加内容
-			LinearLayout ll = new LinearLayout(MainActivity.this);
-			ll.setOrientation(LinearLayout.HORIZONTAL);
-			TextView tv = new TextView(MainActivity.this);
-			tv.setText(nameArray[position]);
-			tv.setTextSize(32);
-			tv.setTextColor(Color.BLACK);
-			tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-			tv.setGravity(Gravity.CENTER_VERTICAL);
-			TextView tv2 = new TextView(MainActivity.this);
-			tv2.setText("["+PWArray[position]+"]");
-			tv2.setTextSize(28);
-			tv2.setTextColor(Color.BLACK);
-			tv2.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-			tv2.setGravity(Gravity.CENTER_VERTICAL);	//设置TextView控件在父容器中的位置
-			TextView tv3 = new TextView(MainActivity.this);
-			tv3.setText("["+idArray[position]+"]");
-			tv3.setTextSize(28);
-			tv3.setTextColor(Color.BLACK);
-			tv3.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-			tv3.setGravity(Gravity.CENTER_VERTICAL);	//设置TextView控件在父容器中的位置
-			ll.addView(tv3);
-			ll.addView(tv);
-			ll.addView(tv2); 
-			
-			return ll;
-		}
-	};
+        
+        public int getCount() {
+            if(nameArray!= null){           //如果姓名数组不为空
+                return nameArray.length;
+            }
+            else {
+                return 0;                   //如果姓名数组为空则返回0
+            }
+        }
+        
+        public Object getItem(int arg0) {
+            return null;
+        }
+        
+        public long getItemId(int arg0) {
+            return 0;
+        }
+        
+        public View getView(int position, View convertView, ViewGroup parent) {   //适配器李添加内容
+            LinearLayout ll = new LinearLayout(MainActivity.this);
+            ll.setOrientation(LinearLayout.HORIZONTAL);
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText(nameArray[position]);
+            tv.setTextSize(32);
+            tv.setTextColor(Color.BLACK);
+            tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            tv.setGravity(Gravity.CENTER_VERTICAL);
+            TextView tv2 = new TextView(MainActivity.this);
+            tv2.setText("["+PWArray[position]+"]");
+            tv2.setTextSize(28);
+            tv2.setTextColor(Color.BLACK);
+            tv2.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            tv2.setGravity(Gravity.CENTER_VERTICAL);    //设置TextView控件在父容器中的位置
+            TextView tv3 = new TextView(MainActivity.this);
+            tv3.setText("["+idArray[position]+"]");
+            tv3.setTextSize(28);
+            tv3.setTextColor(Color.BLACK);
+            tv3.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+            tv3.setGravity(Gravity.CENTER_VERTICAL);    //设置TextView控件在父容器中的位置
+            ll.addView(tv3);
+            ll.addView(tv);
+            ll.addView(tv2); 
+            
+            return ll;
+        }
+    };
     
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		mHelper=new myOpenHelper(this, DB_NAME, null, 1);
-		
-		setRootButton=(Button)this.findViewById(R.id.setRootPW);
-		setRootButton.setOnClickListener(this);
-		addUserButton=(Button)this.findViewById(R.id.addNewUser);
-		addUserButton.setOnClickListener(this);
-		
-		//listView的初始化
-		 lv = (ListView)findViewById(R.id.lv);
-	     lv.setAdapter(myAdapter);
-	     lv.setOnItemClickListener(new OnItemClickListener() {
-				public void onItemClick(AdapterView<?> arg0, View view, int position,
-						long id) {
-//					Intent intent= new Intent(wyf.wpf.Sample_10_6.this,wyf.wpf.DetailActivity.class);
-//					intent.putExtra("cmd", 0);		//0代表查询联系人，1代表添加联系人
-//					intent.putExtra("id", contactsId[position]);
-//					startActivity(intent);
-				}
-		});
-	     
-	     //checkbox的初始化
-	     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);		
-			mIsLockScreenOn = prefs.getBoolean(LOCK_SCREEN_ON_OFF, false);
-			// set checkbox with saved value.
-			mSetOnOff = (CheckBox) this.findViewById(R.id.setonoff);		
-			mSetOnOff.setChecked(mIsLockScreenOn);
-			mSetOnOff.setOnCheckedChangeListener(new OnCheckedChangeListener(){
-				@Override
-				public void onCheckedChanged(CompoundButton buttonView,
-						boolean isChecked) {
-					// TODO Auto-generated method stub
-					if(buttonView.isChecked()){
-						mSetOnOff.setText("Set LockScreen ON. [Now is on]");
-	                    //check and save
-	                    EnableSystemKeyguard(false);
-	            }
-					else{
-						mSetOnOff.setText("Set LockScreen ON. [Now is off]");
-	                    EnableSystemKeyguard(true);
-					}
-				}
-				
-			});
-	     
-	     
-		
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mHelper=new myOpenHelper(this, DB_NAME, null, 1);
+        
+        setRootButton=(Button)this.findViewById(R.id.setRootPW);
+        setRootButton.setOnClickListener(this);
+        addUserButton=(Button)this.findViewById(R.id.addNewUser);
+        addUserButton.setOnClickListener(this);
+        
+        //listView的初始化
+         lv = (ListView)findViewById(R.id.lv);
+         lv.setAdapter(myAdapter);
+         lv.setOnItemClickListener(new OnItemClickListener() {
+                public void onItemClick(AdapterView<?> arg0, View view, int position,
+                        long id) {
+//                  Intent intent= new Intent(wyf.wpf.Sample_10_6.this,wyf.wpf.DetailActivity.class);
+//                  intent.putExtra("cmd", 0);      //0代表查询联系人，1代表添加联系人
+//                  intent.putExtra("id", contactsId[position]);
+//                  startActivity(intent);
+                }
+        });
+         
+         //checkbox的初始化
+         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);     
+            mIsLockScreenOn = prefs.getBoolean(LOCK_SCREEN_ON_OFF, false);
+            // set checkbox with saved value.
+            mSetOnOff = (CheckBox) this.findViewById(R.id.setonoff);        
+            mSetOnOff.setChecked(mIsLockScreenOn);
+            mSetOnOff.setOnCheckedChangeListener(new OnCheckedChangeListener(){
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView,
+                        boolean isChecked) {
+                    // TODO Auto-generated method stub
+                    if(buttonView.isChecked()){
+                        mSetOnOff.setText("Set LockScreen ON. [Now is on]");
+                        //check and save
+                        EnableSystemKeyguard(false);
+                }
+                    else{
+                        mSetOnOff.setText("Set LockScreen ON. [Now is off]");
+                        EnableSystemKeyguard(true);
+                    }
+                }
+                
+            });
+         
+         
+        
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		if(v==setRootButton){
-			Intent i=new Intent(this,setRootPW.class);
-	        startActivity(i); 
-		}
-		else if(v==addUserButton){
-			Intent i=new Intent(this,addUser.class);
-			startActivity(i);
-		}
-	}
-	
-	 @Override
-	protected void onResume() {
-		getInfo();
-		myAdapter.notifyDataSetChanged();
-		super.onResume();
-	}
-	
-	 @Override
-	 protected void onStart() {
-	    // TODO Auto-generated method stub
-	    super.onStart();
-	    startService(new Intent(this, MyLockScreenService.class));
-	 }
-	 
-	 @Override
-	    protected void onStop() {
-	    	// TODO Auto-generated method stub
-	    	super.onStop();
-	    	
-	    	mIsLockScreenOn = mSetOnOff.isChecked();
-	    	
-	    	if(mIsLockScreenOn)
-	    		// keep on disabling the system Keyguard
-	    		EnableSystemKeyguard(false);
-	    	else {
-	    		stopService(new Intent(this, MyLockScreenService.class));
-	    		// recover original Keyguard
-	    		EnableSystemKeyguard(true);
-	    	}
-	    	
-	    	// save the setting before leaving.
-	    	SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
-			editor.putBoolean(LOCK_SCREEN_ON_OFF, mIsLockScreenOn);
-			editor.commit();
-	    	    	
-	    }
-	    
-	    void EnableSystemKeyguard(boolean bEnable){
-	    	KeyguardManager mKeyguardManager=null;
-	    	KeyguardLock mKeyguardLock=null; 
-	    	
-	    	mKeyguardManager = (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);  
-	    	mKeyguardLock = mKeyguardManager.newKeyguardLock(""); 
-	    	if(bEnable)
-	    		mKeyguardLock.reenableKeyguard();
-	    	else
-	    		mKeyguardLock.disableKeyguard();
-	    }
-	    
-	 
-	//从数据库获取信息
-	public void getInfo(){
-		SQLiteDatabase db = mHelper.getWritableDatabase();		//获取数据库连接
-    	Cursor c = db.query(TABLE_NAME, new String[]{ID,NAME,PW}, null, null, null, null, ID);
-    	int idIndex = c.getColumnIndex(ID);
-    	int nameIndex = c.getColumnIndex(NAME);		//获得姓名列的列号
-    	int pwIndex = c.getColumnIndex(PW);
-    	idArray = new int[c.getCount()];			//创建存放id的int数组对象
-    	nameArray = new String[c.getCount()];			//创建存放姓名的String数组对象
-    	PWArray = new String[c.getCount()];	
-    	int i=0;			
-    	for(c.moveToFirst();!(c.isAfterLast());c.moveToNext()){
-    		idArray[i] = c.getInt(idIndex);
-    		nameArray[i] = c.getString(nameIndex);			//将姓名添加到String数组中
-    		PWArray[i] = c.getString(pwIndex);			//将pW添加到String数组中
-    		i++;
-    	}
-    	c.close();				//关闭Cursor对象
-    	db.close();				//关闭SQLiteDatabase对象
-	}
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        if(v==setRootButton){
+            Intent i=new Intent(this,setRootPW.class);
+            startActivity(i); 
+        }
+        else if(v==addUserButton){
+            Intent i=new Intent(this,addUser.class);
+            startActivity(i);
+        }
+    }
+    
+     @Override
+    protected void onResume() {
+        getInfo();
+        myAdapter.notifyDataSetChanged();
+        super.onResume();
+    }
+    
+     @Override
+     protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+        startService(new Intent(this, MyLockScreenService.class));
+     }
+     
+     @Override
+        protected void onStop() {
+            // TODO Auto-generated method stub
+            super.onStop();
+            
+            mIsLockScreenOn = mSetOnOff.isChecked();
+            
+            if(mIsLockScreenOn)
+                // keep on disabling the system Keyguard
+                EnableSystemKeyguard(false);
+            else {
+                stopService(new Intent(this, MyLockScreenService.class));
+                // recover original Keyguard
+                EnableSystemKeyguard(true);
+            }
+            
+            // save the setting before leaving.
+            SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+            editor.putBoolean(LOCK_SCREEN_ON_OFF, mIsLockScreenOn);
+            editor.commit();
+                    
+        }
+        
+        void EnableSystemKeyguard(boolean bEnable){
+            KeyguardManager mKeyguardManager=null;
+            KeyguardLock mKeyguardLock=null; 
+            
+            mKeyguardManager = (KeyguardManager)getSystemService(Context.KEYGUARD_SERVICE);  
+            mKeyguardLock = mKeyguardManager.newKeyguardLock(""); 
+            if(bEnable)
+                mKeyguardLock.reenableKeyguard();
+            else
+                mKeyguardLock.disableKeyguard();
+        }
+        
+     
+    //从数据库获取信息
+    public void getInfo(){
+        SQLiteDatabase db = mHelper.getWritableDatabase();      //获取数据库连接
+        Cursor c = db.query(TABLE_NAME, new String[]{ID,NAME,PW}, null, null, null, null, ID);
+        int idIndex = c.getColumnIndex(ID);
+        int nameIndex = c.getColumnIndex(NAME);     //获得姓名列的列号
+        int pwIndex = c.getColumnIndex(PW);
+        idArray = new int[c.getCount()];            //创建存放id的int数组对象
+        nameArray = new String[c.getCount()];           //创建存放姓名的String数组对象
+        PWArray = new String[c.getCount()]; 
+        int i=0;            
+        for(c.moveToFirst();!(c.isAfterLast());c.moveToNext()){
+            idArray[i] = c.getInt(idIndex);
+            nameArray[i] = c.getString(nameIndex);          //将姓名添加到String数组中
+            PWArray[i] = c.getString(pwIndex);          //将pW添加到String数组中
+            i++;
+        }
+        c.close();              //关闭Cursor对象
+        db.close();             //关闭SQLiteDatabase对象
+    }
 
 }
--- src/com/example/figleaf/MyLock.java
+++ src/com/example/figleaf/MyLock.java
@@ -19,7 +19,7 @@ import android.widget.Toast;
 public class MyLock extends Activity {
 
     Button buttonconfirm;
-    EditText pwd_in_text;	
+    EditText pwd_in_text;   
     int change;              //记录上次改变的位置
     private myOpenHelper mHelper;
     private int[] idArray;
@@ -43,149 +43,149 @@ public class MyLock extends Activity {
     Button.OnClickListener listener = new Button.OnClickListener(){
 
         public void onClick(View v){
-        	int index=-1; //记录命中位置
+            int index=-1; //记录命中位置
             if (v == buttonconfirm){
                 String pwd = pwd_in_text.getText().toString().trim();
                 int count=getInfo();  //获取数据库里一种多少数据
                 for(int i=0;i<count;i++){                 //查找数据是否有这个密码
-                	if(pwd.equals(PWArray[i])){   //如果命中
-                		index=i;
-                		break;
-                	}
+                    if(pwd.equals(PWArray[i])){   //如果命中
+                        index=i;
+                        break;
+                    }
                 }
                 if (index==-1){
-                	Toast.makeText(MyLock.this, "Wrong Password", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MyLock.this, "Wrong Password", Toast.LENGTH_LONG).show();
                 } 
                 else{ 
-                	if(index!=change){             //如果换密码了
-                		Log.i("recover",String.valueOf(change));
-                		if(change!=0){
-                			recover(); //恢复被隐藏
-                		} 
-                		dirtyArray[change]="0";            //置零
-                		update(idArray[change],change);//更新数据
-                		
-                		
-                		if(index!=0){
-                			hidefile(index); 
-                		}                    //隐藏文件
-                		dirtyArray[index]="1";
-                		update(idArray[index],index);//更新数据库信息
-                		
-                	}
-                	                             
-                	System.exit(0);
+                    if(index!=change){             //如果换密码了
+                        Log.i("recover",String.valueOf(change));
+                        if(change!=0){
+                            recover(); //恢复被隐藏
+                        } 
+                        dirtyArray[change]="0";            //置零
+                        update(idArray[change],change);//更新数据
+                        
+                        
+                        if(index!=0){
+                            hidefile(index); 
+                        }                    //隐藏文件
+                        dirtyArray[index]="1";
+                        update(idArray[index],index);//更新数据库信息
+                        
+                    }
+                                                 
+                    System.exit(0);
                 }
                
  
-                	
+                    
           }
 
         }
     };
 
     public int  getInfo(){
-		SQLiteDatabase db = mHelper.getWritableDatabase();		//获取数据库连接
-    	Cursor c = db.query(TABLE_NAME, new String[]{ID,NAME,PW,PICPATH,DIRTY}, null, null, null, null, ID);
-    	int idIndex = c.getColumnIndex(ID);
-    	int nameIndex=c.getColumnIndex(NAME);
-    	int pwIndex = c.getColumnIndex(PW);		//获得姓名列的列号
-    	int picIndex = c.getColumnIndex(PICPATH);
-    	int dirIndex =c.getColumnIndex(DIRTY);
-    	idArray = new int[c.getCount()];			//创建存放id的int数组对象
-    	PWArray = new String[c.getCount()];	
-    	picPathArray = new String[c.getCount()];			//创建存放图片地址的String数组对象
-    	nameArray=new String[c.getCount()];
-    	dirtyArray=new String[c.getCount()];
-    	int i=0;			
-    	for(c.moveToFirst();!(c.isAfterLast());c.moveToNext()){
-    		idArray[i] = c.getInt(idIndex);
-    		nameArray[i]=c.getString(nameIndex);
-    		PWArray[i] = c.getString(pwIndex);			//将pW添加到String数组中
-    		picPathArray[i] = c.getString(picIndex);			//将路径添加到String数组中
-    		dirtyArray[i]=c.getString(dirIndex);
-    		Log.i(nameArray[i],dirtyArray[i]);
-    		if(dirtyArray[i].equals("1")){
-    			change=i;
-    			Log.i("getInfo",String.valueOf(change));
-    		}
-    			             //记录上次使用的用户位置
-    		i++;
-    	}
-    	c.close();				//关闭Cursor对象
-    	db.close();				//关闭SQLiteDatabase对象
-    	return i;
-	}
+        SQLiteDatabase db = mHelper.getWritableDatabase();      //获取数据库连接
+        Cursor c = db.query(TABLE_NAME, new String[]{ID,NAME,PW,PICPATH,DIRTY}, null, null, null, null, ID);
+        int idIndex = c.getColumnIndex(ID);
+        int nameIndex=c.getColumnIndex(NAME);
+        int pwIndex = c.getColumnIndex(PW);     //获得姓名列的列号
+        int picIndex = c.getColumnIndex(PICPATH);
+        int dirIndex =c.getColumnIndex(DIRTY);
+        idArray = new int[c.getCount()];            //创建存放id的int数组对象
+        PWArray = new String[c.getCount()]; 
+        picPathArray = new String[c.getCount()];            //创建存放图片地址的String数组对象
+        nameArray=new String[c.getCount()];
+        dirtyArray=new String[c.getCount()];
+        int i=0;            
+        for(c.moveToFirst();!(c.isAfterLast());c.moveToNext()){
+            idArray[i] = c.getInt(idIndex);
+            nameArray[i]=c.getString(nameIndex);
+            PWArray[i] = c.getString(pwIndex);          //将pW添加到String数组中
+            picPathArray[i] = c.getString(picIndex);            //将路径添加到String数组中
+            dirtyArray[i]=c.getString(dirIndex);
+            Log.i(nameArray[i],dirtyArray[i]);
+            if(dirtyArray[i].equals("1")){
+                change=i;
+                Log.i("getInfo",String.valueOf(change));
+            }
+                             //记录上次使用的用户位置
+            i++;
+        }
+        c.close();              //关闭Cursor对象
+        db.close();             //关闭SQLiteDatabase对象
+        return i;
+    }
     
     protected void recover(){
-    	
-    	if(!picPathArray[change].equals("0")&&!picPathArray[change].equals(null)){//防止没有内容
-    		Log.i("recover",picPathArray[change]);
-    		String path=picPathArray[change];
-    		File file=new File(path);
-    		String newpath=file.getParentFile().getParent()+"/"+file.getName();  //改成正确路径路径
-    		picPathArray[change]=newpath;   //恢复正确地址
-    		Log.i("recover",newpath);
-    		File newfile=new File(newpath);
-    		file.renameTo(newfile);
-    		file.delete();
-    		Uri localUri = Uri.fromFile(newfile);//广播消息，刷新相册
-    		Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
-    		sendBroadcast(localIntent);
-    		
-    	}
+        
+        if(!picPathArray[change].equals("0")&&!picPathArray[change].equals(null)){//防止没有内容
+            Log.i("recover",picPathArray[change]);
+            String path=picPathArray[change];
+            File file=new File(path);
+            String newpath=file.getParentFile().getParent()+"/"+file.getName();  //改成正确路径路径
+            picPathArray[change]=newpath;   //恢复正确地址
+            Log.i("recover",newpath);
+            File newfile=new File(newpath);
+            file.renameTo(newfile);
+            file.delete();
+            Uri localUri = Uri.fromFile(newfile);//广播消息，刷新相册
+            Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
+            sendBroadcast(localIntent);
+            
+        }
     
-    	
+        
     }
     protected void hidefile(int i){
-    	if(!picPathArray[i].equals("0")&&!picPathArray[i].equals(null)){
-    		File file = new File(picPathArray[i]);                //根据路径，获得文件
-    		String fpath=file.getParentFile().getPath();
-    		File newfile = new File(fpath+"/.hide/"+file.getName());   //创立隐藏文件夹
-	        file.renameTo(newfile); 
-	        String s=newfile.getPath();
-	        picPathArray[i]=s;                   //更新文件路径
-	        Log.i("hideFile",s);
-	        file.delete();	
-	        Uri localUri = Uri.fromFile(file);//广播消息，刷新相册
-	        Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
-	        sendBroadcast(localIntent);
-	      
-    	}
-    	
-    	
+        if(!picPathArray[i].equals("0")&&!picPathArray[i].equals(null)){
+            File file = new File(picPathArray[i]);                //根据路径，获得文件
+            String fpath=file.getParentFile().getPath();
+            File newfile = new File(fpath+"/.hide/"+file.getName());   //创立隐藏文件夹
+            file.renameTo(newfile); 
+            String s=newfile.getPath();
+            picPathArray[i]=s;                   //更新文件路径
+            Log.i("hideFile",s);
+            file.delete();  
+            Uri localUri = Uri.fromFile(file);//广播消息，刷新相册
+            Intent localIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, localUri);
+            sendBroadcast(localIntent);
+          
+        }
+        
+        
     }
     
     protected void update(int id,int i){
-		SQLiteDatabase db = mHelper.getWritableDatabase();		//获得数据库对象
-		ContentValues values = new ContentValues();
-		values.put(NAME, nameArray[i]);
-		values.put(PW, PWArray[i]);
-		values.put(PICPATH, picPathArray[i]);
-		values.put(DIRTY, dirtyArray[i]);
-		
-		db.update(TABLE_NAME, values, ID+"=?", new String[]{id+""});	//更新数据
-		db.close();
-    	
+        SQLiteDatabase db = mHelper.getWritableDatabase();      //获得数据库对象
+        ContentValues values = new ContentValues();
+        values.put(NAME, nameArray[i]);
+        values.put(PW, PWArray[i]);
+        values.put(PICPATH, picPathArray[i]);
+        values.put(DIRTY, dirtyArray[i]);
+        
+        db.update(TABLE_NAME, values, ID+"=?", new String[]{id+""});    //更新数据
+        db.close();
+        
     }
     
     
     //@Override
-	//public boolean dispatchKeyEvent(KeyEvent event) {
-		// TODO Auto-generated method stub
-		//return super.dispatchKeyEvent(event);
-	//	return true;
-	//}
+    //public boolean dispatchKeyEvent(KeyEvent event) {
+        // TODO Auto-generated method stub
+        //return super.dispatchKeyEvent(event);
+    //  return true;
+    //}
     
-	
-	@Override
-	public void onAttachedToWindow() {
-		// TODO Auto-generated method stub
-		
-		// try to disable home key.
-		//this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
-		//super.onAttachedToWindow();
-	}
-	
+    
+    @Override
+    public void onAttachedToWindow() {
+        // TODO Auto-generated method stub
+        
+        // try to disable home key.
+        //this.getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD);
+        //super.onAttachedToWindow();
+    }
+    
     
 }
